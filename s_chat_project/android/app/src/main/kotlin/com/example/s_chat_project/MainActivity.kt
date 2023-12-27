89a90a38adce6ff3deaa7f19ca5f9b3fd65d22a7
SeifeddineMechergui <saifmechergui2222@gmail.com>
2023-12-27 16:48:37 +0100

    terminate all projects

--- s_chat_project/android/app/src/main/kotlin/com/example/s_chat_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.s_chat_project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ store/lib/screens/update_product_page.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:store/models/product_model.dart';
+import 'package:store/services/update_product.dart';
+import 'package:store/widgets/custom_button.dart';
+import 'package:store/widgets/custom_text_field.dart';
+
+class UpdateProductPage extends StatefulWidget {
+  static String id = 'update product';
+
+  @override
+  State<UpdateProductPage> createState() => _UpdateProductPageState();
+}
+
+class _UpdateProductPageState extends State<UpdateProductPage> {
+  String? productName, desc, image;
+
+  String? price;
+  bool isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    ProductModel product =
+        ModalRoute.of(context)!.settings.arguments as ProductModel;
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Update Product',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 100,
+                ),
+                CustomTextField(
+                  onChanged: (data) {
+                    productName = data;
+                  },
+                  hintText: 'Product Name',
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  onChanged: (data) {
+                    desc = data;
+                  },
+                  hintText: 'description',
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  onChanged: (data) {
+                    price = data;
+                  },
+                  hintText: 'price',
+                  inputType: TextInputType.number,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  onChanged: (data) {
+                    image = data;
+                  },
+                  hintText: 'image',
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                CustomButon(
+                  text: 'Update',
+                  onTap: () async {
+                    isLoading = true;
+                    setState(() {});
+                    try {
+                      await updateProduct(product);
+
+                      print('success');
+                    } catch (e) {
+                      print(e.toString());
+                    }
+                    isLoading = false;
+                    setState(() {});
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> updateProduct(ProductModel product) async {
+    await UpdateProductService().updateProduct(
+        id: product.id,
+        title: productName == null ? product.title : productName!,
+        price: price == null ? product.price.toString() : price!,
+        desc: desc == null ? product.description : desc!,
+        image: image == null ? product.image : image!,
+        category: product.category);
+  }
+}
